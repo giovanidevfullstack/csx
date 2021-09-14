a77979d28740c78c3ec70ff758557a7274829e4d
Giovani Souza <giovani.devfullstack@gmail.com>
2021-09-14 09:15:14 -0300

    Added: User [migration, factory, seeder]

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -23,11 +24,18 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastname(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => Hash::make('password'),
+            'phone' => $this->faker->phoneNumber(),
+            'age' => rand(1,3),
+            'gender' => array_rand(['m', 'f']),
+            'is_admin' => rand(0,1),
+            'is_seller' => rand(0,1),
+            'is_employee' => rand(0,1),
+            'remember_token' => Str::random(10)
         ];
     }
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,19 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone', 20)->nullable();
+            $table->string('age', 3)->nullable();
+            $table->string('gender', 3)->nullable();
+            $table->boolean('is_admin')->nullable();
+            $table->boolean('is_seller')->nullable();
+            $table->boolean('is_employee')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
