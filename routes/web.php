2795338d55d3e20416b089224a3f0d6307dabc1e
Giovani Souza <giovani.devfullstack@gmail.com>
2021-09-22 15:30:51 -0300

    Added: routes to middleware auth

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,15 @@ Route::get('/', function () {
 //landing routes
 
 //protected routes
-Route::get('/store', function() {
-    return view('dashboard.store.index');
-})->name('dashboard.store.index');
+Route::middleware(['auth'])->group(function (){
+    Route::get('/store', function() {
+        return view('dashboard.store.index');
+    })->name('dashboard.store.index');
 
-Route::get('/store/vehicles', function() {
-    return view('dashboard.store.vehicles.index');
-})->name('dashboard.store.vehicles.index');
+    Route::get('/store/vehicles', function() {
+        return view('dashboard.store.vehicles.index');
+    })->name('dashboard.store.vehicles.index');
+});
 
-
-/** 
- * Todo: Auth 
- */
 //auth routes
 require __DIR__.'/auth.php';
