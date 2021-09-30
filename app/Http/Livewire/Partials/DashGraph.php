9225cb1b1c94fd50d1ed0dbb090cb5df81c069c5
Giovani Souza <giovani.devfullstack@gmail.com>
2021-09-30 09:19:42 -0300

    Ref: Mock for create the main graph from the component

--- app/Http/Livewire/Partials/DashGraph.php
+++ app/Http/Livewire/Partials/DashGraph.php
@@ -13,7 +13,52 @@ class DashGraph extends Component
     public $labels = ['April', 'March', 'May', 'Jun', 'Jul', 'Aug'];
 
     //? test
-    public $title = "Teste";
+    public $datasets = [
+        [
+            'label' => 'Salles',
+            'data' => [21, 59, 33, 56, 12, 143],
+            'backgroundColor' => [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            'borderColor' => [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            'borderWidth' => 1
+
+        ],
+        [
+            'label' => 'Deals',
+            'data' => [5, -19, 32, 15, 72, 113],
+            'backgroundColor' => [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            'borderColor' => [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            'borderWidth' => 1
+
+        ]
+    ];
 
     public function render()
     {
--- resources/views/livewire/partials/dash-graph.blade.php
+++ resources/views/livewire/partials/dash-graph.blade.php
@@ -30,68 +30,22 @@
     <script>
         document.addEventListener('livewire:load', function() { 
             //get all props
-            const title = @this.get('title');
-            const labels = @this.get('labels');
-            const salles = @this.get('salles');
-            const deals = @this.get('deals');
-            
+            const labels = @this.get('labels');            
+            const datasets = @this.get('datasets');
+
             //build graph
             var ctx = document.getElementById('myChart').getContext('2d');
             var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: labels,
-                    datasets: [
-                    {
-                        //construção do dataset tem que ser dinamico
-                        //como?
-                        label: title,
-                        data: salles,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: title,
-                        data: deals,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
+                    datasets: datasets
                 },
                 options: {
                     responsive: true,
                     scales: {
                         y: {
-                            beginAtZero: true
+                            beginAtZero: false
                         }
                     }
                 }
