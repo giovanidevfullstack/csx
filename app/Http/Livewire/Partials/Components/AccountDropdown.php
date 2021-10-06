b2a31e2818aff77ba90e5bed9a92a621da048bfb
Giovani Souza <giovani.devfullstack@gmail.com>
2021-10-06 09:57:40 -0300

    Fix: On toggle dark redirect to actual route

--- app/Http/Livewire/Partials/Components/AccountDropdown.php
+++ app/Http/Livewire/Partials/Components/AccountDropdown.php
@@ -4,11 +4,19 @@ namespace App\Http\Livewire\Partials\Components;
 
 use Livewire\Component;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class AccountDropdown extends Component
 {
     protected $listeners = ['toggled' => 'toggleDark'];
+
+    public $route;
     
+    public function mount()
+    {
+        $this->route = Route::currentRouteName();
+    }
+
     public function render()
     {
         return view('livewire.partials.components.account-dropdown');
@@ -20,6 +28,6 @@ class AccountDropdown extends Component
 
         $request->session()->put('isDark', !$isDark);
 
-        return redirect()->to(route('dashboard.store.index'));
+        return redirect()->to(route($this->route));
     }
 }
