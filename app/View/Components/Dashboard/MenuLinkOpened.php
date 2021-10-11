de3c760c65aa7543bd1d695a1610060e560e7724
Giovani Souza <giovani.devfullstack@gmail.com>
2021-10-11 16:28:12 -0300

    Added: Menu links as blade components

---
+++ app/View/Components/Dashboard/MenuLinkOpened.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components\Dashboard;
+
+use App\Models\Menu;
+use Illuminate\View\Component;
+
+class MenuLinkOpened extends Component
+{
+
+    public $menu;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(Menu $menu)
+    {
+        $this->menu = $menu;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.dashboard.menu-link-opened');
+    }
+}
--- resources/views/livewire/dashboard/main-nav.blade.php
+++ resources/views/livewire/dashboard/main-nav.blade.php
@@ -22,53 +22,10 @@
                     @endif
                     
                     <div x-show="open">
-                        <!-- <menu-link open:false/>-->
-                        <div class="ml-2">
-                            <a href="{{ is_null($menu->route) ?  '#' : route($menu->route) }}" 
-                                class="py-2 m-1 flex justify-between text-sm
-                                text-gray-500 border-l-2 border-gray-200
-                                hover:text-indigo-400 hover:border-indigo-400
-                                dark:border-gray-800 dark:hover:border-gray-300 dark:hover:text-gray-300 dark:text-gray-700 
-                                {{ (Route::current()->getName() == $menu->route) ? 'text-indigo-400 border-indigo-400' : ''}}">
-                    
-                                <div class="flex items-center pl-3 w-full">
-                                    <div class="mr-3 w-5">
-                                        <i class="fas {{ $menu->icon }}"></i>
-                                    </div>
-                    
-                                    <div class="w-auto">
-                                        <span class="">{{$menu->name}}</span>
-                                    </div>
-                                </div>
-                    
-                                <div class="flex items-center">
-                                    @if (!empty($menu->new_msgs))
-                                        <span class="bg-red-400 text-white text-xs font-light rounded-full w-4 h-4 block text-center items-center
-                                            dark:bg-gray-600">
-                                            {{ $menu->new_msgs }}
-                                        </span>
-                                    @endif
-                                </div>
-                            </a>
-                        </div>
+                        <x-dashboard.menu-link-opened :menu="$menu" open=true/>
                     </div>
                     <div x-show="!open">
-                        <!-- <menu-link open:true/>-->
-                        <a href="{{ is_null($menu->route) ?  '#' : route($menu->route) }}" 
-                            class="w-full h-10 flex justify-center items-center text-md
-                            text-gray-500 dark:text-gray-700 
-                            hover:text-indigo-400 dark:hover:text-gray-300
-                            {{ (Route::current()->getName() == $menu->route) ? 'text-indigo-400' : ''}}">
-                    
-                            <i class="fas {{ $menu->icon }}"></i>
-                    
-                            @if (!empty($menu->new_msgs))
-                                <span class="bg-red-400 absolute w-4 h-4 ml-5 mb-5 items-center rounded-full text-center text-white text-xs font-light
-                                    dark:bg-gray-600">
-                                    {{ $menu->new_msgs }}
-                                </span>
-                            @endif
-                        </a>
+                        <x-dashboard.menu-link-closed :menu="$menu"/> 
                     </div>
                 @endforeach
 
@@ -81,51 +38,10 @@
                         @endif
                         
                         <div x-show="open">
-                            <div class="ml-2">
-                                <a href="{{ is_null($menu->route) ?  '#' : route($menu->route) }}" 
-                                    class="py-2 m-1 flex justify-between text-sm
-                                    text-gray-500 border-l-2 border-gray-200
-                                    hover:text-indigo-400 hover:border-indigo-400
-                                    dark:border-gray-800 dark:hover:border-gray-300 dark:hover:text-gray-300 dark:text-gray-700 
-                                    {{ (Route::current()->getName() == $menu->route) ? 'text-indigo-400 border-indigo-400' : ''}}">
-                        
-                                    <div class="flex items-center pl-3 w-full">
-                                        <div class="mr-3 w-5">
-                                            <i class="fas {{ $menu->icon }}"></i>
-                                        </div>
-                        
-                                        <div class="w-auto">
-                                            <span class="">{{$menu->name}}</span>
-                                        </div>
-                                    </div>
-                        
-                                    <div class="flex items-center">
-                                        @if (!empty($menu->new_msgs))
-                                            <span class="bg-red-400 text-white text-xs font-light rounded-full w-4 h-4 block text-center items-center
-                                                dark:bg-gray-600">
-                                                {{ $menu->new_msgs }}
-                                            </span>
-                                        @endif
-                                    </div>
-                                </a>
-                            </div>
+                            <x-dashboard.menu-link-opened :menu="$menu"/>
                         </div>
                         <div x-show="!open">
-                            <a href="{{ is_null($menu->route) ?  '#' : route($menu->route) }}" 
-                                class="w-full h-10 flex justify-center items-center text-md
-                                text-gray-500 dark:text-gray-700 
-                                hover:text-indigo-400 dark:hover:text-gray-300
-                                {{ (Route::current()->getName() == $menu->route) ? 'text-indigo-400' : ''}}">
-                        
-                                <i class="fas {{ $menu->icon }}"></i>
-                        
-                                @if (!empty($menu->new_msgs))
-                                    <span class="bg-red-400 absolute w-4 h-4 ml-5 mb-5 items-center rounded-full text-center text-white text-xs font-light
-                                        dark:bg-gray-600">
-                                        {{ $menu->new_msgs }}
-                                    </span>
-                                @endif
-                            </a>                            
+                            <x-dashboard.menu-link-closed :menu="$menu"/>   
                         </div>
                     @endforeach
                 @endcan
